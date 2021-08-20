f109b2120a9536610e7581e1187dae9cc859d8dd
Rasheed <lawalabdulrasheedbolaji@gmail.com>
2021-08-20 11:28:55 +0100

    update prod

--- i_resume/settings.py
+++ i_resume/settings.py
@@ -21,9 +21,6 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# refresh whitenoise
-WHITENOISE_AUTOREFRESH = True
-
 
 ALLOWED_HOSTS = ['*']
 
